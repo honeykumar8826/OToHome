be75b1e5bd3dd7f8880a6b098ef9880998df9182
Harish kumar <harishkumar8826@gmail.com>
2019-09-09 02:36:16 +0530

    09/09/2019

---
+++ app/src/main/java/com/travel/cab/service/TestActivity.java
@@ -0,0 +1,14 @@
+package com.travel.cab.service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
